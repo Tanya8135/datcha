114110a7fe358ca46f59bc90251ab5931aa8a725
tanya8135 <108739694+Tanya8135@users.noreply.github.com>
2023-03-10 00:37:32 +0200

    add slider
    И он работает!!!)))

--- js/slider.js
+++ js/slider.js
@@ -1,69 +1,21 @@
-// var sliderIndex = 1;
-// showSlider(sliderIndex);
-
-// function changeSlider(n) {
-//   showSlider(sliderIndex += n);
-// }
-
-// function showSlider(n) {
-//   var i;
-//   var x = document.getElementsByClassName("slider");
-//   if (n > x.length) {sliderIndex = 1}
-//   if (n < 1) {sliderIndex = x.length}
-//   for (i = 0; i < x.length; i++) {
-//     x[i].style.display = "none";
-//   }
-//   x[sliderIndex-1].style.display = "block";
-// }
-
-
-const sliders = document.querySelectorAll('.slider');
-
-// Добавляем обработчики на кнопки и событие "touchstart" на мобильных устройствах
-sliders.forEach((slider) => {
-  const images = slider.querySelectorAll('img');
-  let currentImage = 0;
-  let startX;
-
-  const showImage = (n) => {
-    images.forEach((image) => {
-      image.style.display = 'none';
+$(document).ready(function() {
+    $('.slider-room').slick({
+      slidesToShow: 1,
+      slidesToScroll: 1,
+      arrows: true,
+      prevArrow: $('.arrow-room-l'),
+      nextArrow: $('.arrow-room-r'),
+      swipe: true
     });
-    images[n].style.display = 'block';
-    currentImage = n;
-  };
-
-  const prevImage = () => {
-    if (currentImage === 0) {
-      showImage(images.length - 1);
-    } else {
-      showImage(currentImage - 1);
-    }
-  };
-
-  const nextImage = () => {
-    if (currentImage === images.length - 1) {
-      showImage(0);
-    } else {
-      showImage(currentImage + 1);
-    }
-  };
-
-  slider.previousElementSibling.addEventListener('click', prevImage);
-  slider.nextElementSibling.addEventListener('click', nextImage);
-
-  slider.addEventListener('touchstart', (e) => {
-    startX = e.changedTouches[0].clientX;
   });
 
-  slider.addEventListener('touchend', (e) => {
-    const endX = e.changedTouches[0].clientX;
-    if (startX - endX > 50) {
-      nextImage();
-    } else if (endX - startX > 50) {
-      prevImage();
-    }
+  $(document).ready(function() {
+    $('.slider-general').slick({
+      slidesToShow: 1,
+      slidesToScroll: 1,
+      arrows: true,
+      prevArrow: $('.arrow-general-l'),
+      nextArrow: $('.arrow-general-r'),
+      swipe: true
+    });
   });
-
-  showImage(currentImage);
-});
