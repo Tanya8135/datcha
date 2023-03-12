25e8d6b124072ced168c9d50dc7e94ce3dcf24d1
tanya8135 <108739694+Tanya8135@users.noreply.github.com>
2023-03-12 14:54:56 +0200

    ничего

--- js/language.js
+++ js/language.js
@@ -1,8 +1,3 @@
-const selectElement = document.querySelector('.select');
-
-selectElement.addEventListener('change', (event) => {
-  const language = event.target.value;
-  const currentUrl = window.location.href;
-  const url = currentUrl.replace(/\/(UK|RU)\//, `/${language}/`);
-  window.location.href = url;
-});
+document.querySelector('#button-9 .knobs').addEventListener('click', function() {
+  window.location.href = 'rus.html'; // перенаправляем пользователя на страницу
+  });
